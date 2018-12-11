15da4733fbda4266edc2cdda342258273edf1f2b
Mr.yan <40586009+Mryan2005@users.noreply.github.com>
2018-12-11 17:35:32 +0800

    add close

--- function_table.py
+++ function_table.py
@@ -7,5 +7,11 @@ def begin() :
         print('hello world')
     elif c == 2:
         print('hi')
+def close():
+    c = random.randint(1,2)
+    if c == 1:
+        print('goodbye!')
+    elif c == 2:
+        print('see you again!')
 # Get system information
 platform = sys.platform
