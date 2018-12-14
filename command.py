769d715465322c9525d036c525e3119d2ed479de
Mr.yan <40586009+Mryan2005@users.noreply.github.com>
2018-12-15 06:38:22 +0800

    add while

--- command.py
+++ command.py
@@ -3,20 +3,22 @@ import sys
 # main
 def main():
     mode=0
-    command = input()
-    if command == 'su':
-        if mode == 0:
-            print('Please input password')
-        password=input()
-        if password == '10000':
-            print('welcome')
-            mode = 1
+    true=1
+    while true == 1:
+        command = input()
+        if command == 'su':
+            if mode == 0:
+                print('Please input password')
+            password=input()
+            if password == '10000':
+                print('welcome')
+                mode = 1
+            else:
+                print('Password error!')
         else:
-            print('Password error!')
-    else:
-        print('you are root')
-    if command == 'stop':
-        stop()
+            print('you are root')
+        if command == 'stop':
+            stop()
 # stop 
 def stop():
     if mode == 0:
