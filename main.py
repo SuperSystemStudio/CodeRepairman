b1a92d9fd9fdca2bb692db8241e3917352708bd5
Mr.yan <40586009+Mryan2005@users.noreply.github.com>
2018-12-23 08:03:46 +0800

    add in

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def sshserver():
     while True: #第一层loop
         print("Waiting for client connection")
         conn,addr = server.accept() #接受并建立与客户端的连接,程序在此处开始阻塞,只到有客户端连接进来...
-        print("[system]>>>a user join our server,it is",addr )
+        print("[system]>>>a user join in our server,it is",addr)
         while True:
             data = conn.recv(1024)
             if not data:
